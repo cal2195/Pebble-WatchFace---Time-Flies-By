bb5d6fb29b5db81f45e086c1432bf5e487a5b260
cal2195 <cmartin7@tcd.ie>
2015-08-27 16:05:27 +0100

    Added airplane & time!

--- src/main.c
+++ src/main.c
@@ -6,21 +6,86 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-static GBitmap *s_city_bitmap;
-static BitmapLayer *s_bitmap_background_layer;
+static TextLayer *s_time_layer;
+static GFont s_time_font;
+static GBitmap *s_city_bitmap, *s_airplane_bitmap;
+static BitmapLayer *s_bitmap_background_layer, *s_bitmap_airplane_layer;
+int airplane_height;
 
 static void main_window_load(Window *window) {
   // Create Window's child Layers here
+  
+  // City Background
   s_city_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CITY_BACKGROUND);
   s_bitmap_background_layer = bitmap_layer_create(GRect(0, 20, 144, 104));
   bitmap_layer_set_bitmap(s_bitmap_background_layer, s_city_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bitmap_background_layer));
+  
+  // Airplane
+  s_airplane_bitmap = gbitmap_create_with_resource(RESOURCE_ID_AIRPLANE);
+  s_bitmap_airplane_layer = bitmap_layer_create(GRect(0, 10, 10, 3));
+  bitmap_layer_set_bitmap(s_bitmap_airplane_layer, s_airplane_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bitmap_airplane_layer));
+  
+  //Time display
+  s_time_layer = text_layer_create(GRect(0, 112, 144, 58));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_DIGITAL_KAUNO_FONT_65));
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+}
+
+static void update_time() {
+  //get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  airplane_height = rand() % 31;
+  
+  //update time
+  static char time_text[] = "00:00";
+  if(clock_is_24h_style() == true) {
+    //use 24h time
+     strftime(time_text, sizeof("00:00"), "%H:%M", tick_time);
+  }
+  else{
+    //12h time
+    strftime(time_text, sizeof("00:00"), "%I:%M", tick_time);
+  }
+  text_layer_set_text(s_time_layer, time_text);
+}
+
+static void update_airplane() {
+  //get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  //update seconds
+  int x = (2.58*tick_time->tm_sec) - 10;
+  layer_set_frame(bitmap_layer_get_layer(s_bitmap_airplane_layer), GRect(x, airplane_height, 10, 3));
+  
+  if(tick_time->tm_sec==0) {
+    update_time();
+  }
+}
+
+static void tick_handler(struct tm *ticktime, TimeUnits units_changed) {
+  update_airplane();
 }
 
 static void main_window_unload(Window *window) {
   // Destroy Window's child Layers here
+  // Destroy City
   gbitmap_destroy(s_city_bitmap);
   bitmap_layer_destroy(s_bitmap_background_layer);
+  // Destroy Airplane
+  gbitmap_destroy(s_airplane_bitmap);
+  bitmap_layer_destroy(s_bitmap_airplane_layer);
+  // Destroy Textlayers
+  text_layer_destroy(s_time_layer);
 }
 
 static void init() {
@@ -32,6 +97,13 @@ static void init() {
   });
   window_set_background_color(s_main_window, GColorBlack);
   window_stack_push(s_main_window, true);
+  
+  // Set airplane position
+  update_time();
+  update_airplane();
+  
+  //register with TickTimerService
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void deinit() {
