880a2817374d6a921a8616a0a19306f89d73d8ac
cal2195 <cmartin7@tcd.ie>
2015-08-28 16:34:33 +0100

    Added weather checking!

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
  */
 
 #include <pebble.h>
+#define KEY_WEATHER 0
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
@@ -12,7 +13,8 @@ static GBitmap *s_city_bitmap_black, *s_city_bitmap_white, *s_airplane_bitmap, *
 static Layer *s_background_layer, *s_airplane_layer, *s_clouds_layer;
 static InverterLayer *s_invert_layer;
 int airplane_height;
-bool daytime = false;
+bool daytime = false, clouds = false;
+static char weather_buffer[32];
 
 static void draw_city(Layer *layer, GContext *ctx) {
   // Draw white
@@ -67,12 +69,12 @@ static void main_window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), s_clouds_layer);
   
   //Time display
-  s_time_layer = text_layer_create(GRect(0, 112, 144, 58));
+  s_time_layer = text_layer_create(GRect(0, 115, 144, 58));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_DIGITAL_KAUNO_FONT_65));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_MONOSPACE_FONT_40));
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
@@ -124,6 +126,20 @@ static void update_airplane() {
 
 static void tick_handler(struct tm *ticktime, TimeUnits units_changed) {
   update_airplane();
+  
+  // Get weather update every 30 minutes
+  if(ticktime->tm_min % 30 == 0 && ticktime->tm_sec == 0) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Checking Weather!");
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+  
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+  
+    // Send the message!
+    app_message_outbox_send();
+  }
 }
 
 static void main_window_unload(Window *window) {
@@ -146,6 +162,59 @@ static void main_window_unload(Window *window) {
   fonts_unload_custom_font(s_time_font);
 }
 
+static void update_weather()
+{
+  if (clouds)
+  {
+    layer_set_hidden(s_clouds_layer, false);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Showing Clouds!");
+  }
+  else
+  {
+    layer_set_hidden(s_clouds_layer, true);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "NOT Showing Clouds!");
+  }
+}
+
+// Appmessage
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_WEATHER:
+    snprintf(weather_buffer, sizeof(weather_buffer), "%s", t->value->cstring);
+      break;
+    default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+  
+  clouds = (strcmp(weather_buffer, "Clouds") == 0);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "CLOUDS: %d", clouds);
+  update_weather();
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+
 static void init() {
   // Create main Window
   s_main_window = window_create();
@@ -156,9 +225,19 @@ static void init() {
   window_set_background_color(s_main_window, GColorBlack);
   window_stack_push(s_main_window, true);
   
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  
   // Set airplane position
   update_time();
   update_airplane();
+  update_weather();
   
   //register with TickTimerService
   tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
